91d6d21835bed1b0e565741b3820cc0e4ddf685c
nccfsl <niccolofasolo@gmail.com>
2020-05-07 11:31:41 +0200

    bug fixes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/covid-19-local';
+$config['base_url'] = 'https://fasolo-covid-19.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
